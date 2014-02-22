769e1e2877d96027e554ef41214fa91aa407996f
Weihang Fan <wfan@exeter.edu>
2014-02-21 23:57:42 -0500

    server conf

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ var rack        = hat.rack();
 var sharejs     = require('share');
 var livedb      = require('livedb');
 var livedbMongo = require('livedb-mongo');
-var backend     = livedb.client(livedbMongo('localhost:27017/text?auto_reconnect', {
+var connection_string = '127.0.0.1:27017/openide';
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+    connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+    process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+    process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+    process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+    process.env.OPENSHIFT_APP_NAME;
+}
+var backend     = livedb.client(livedbMongo(connection_string, {
     safe: false
 }));
 var share       = sharejs.server.createClient({
