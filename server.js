402d4dce5a30390219d0c64b1fbb200c480ececa
Weihang Fan <wfan@exeter.edu>
2014-03-03 16:27:41 +0000

    completed migration to nodejs

--- server.js
+++ server.js
@@ -1,239 +1,238 @@
-#!/usr/bin/env node
+#!/usr/local/bin/node
 var express     = require('express');
 var fs          = require('fs');
 var kue         = require('kue');
-var jobs        = kue.createQueue({
-    redis: {
-        port: process.env.OPENSHIFT_REDIS_PORT,
-        host: process.env.OPENSHIFT_REDIS_HOST,
-        auth: process.env.REDIS_PASSWORD
-    }
-});
-console.log(process.env.OPENSHIFT_REDIS_PORT);
+var jobs        = kue.createQueue();
 var hat         = require('hat');
 var rack        = hat.rack();
 var sharejs     = require('share');
 var livedb      = require('livedb');
 var livedbMongo = require('livedb-mongo');
-var connection_string = '127.0.0.1:27017/openide';
-if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
-    connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-    process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-    process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-    process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-    process.env.OPENSHIFT_APP_NAME;
-}
-var backend     = livedb.client(livedbMongo(connection_string, {
-    safe: true
+var backend     = livedb.client(livedbMongo('127.0.0.1:27017/app', {
+  safe: true
 }));
 var share       = sharejs.server.createClient({
-    backend: backend
+  backend: backend
 });
 var browserChannel = require('browserchannel').server;
 var Duplex      = require('stream').Duplex;
 var spawn       = require('child_process').spawn;
+var exec        = require('child_process').exec;
+var mongoclient = require('mongodb').MongoClient;
 
 
 /**
- *  Define the sample application.
- */
-var SampleApp = function() {
+*  Define the application.
+*/
+var App = function() {
 
-    //  Scope.
-    var self = this;
+  //  Scope.
+  var self = this;
 
 
-    /*  ================================================================  */
-    /*  Helper functions.                                                 */
-    /*  ================================================================  */
+  /*  ================================================================  */
+  /*  Helper functions.                                                 */
+  /*  ================================================================  */
 
-    /**
-     *  Set up server IP address and port # using env variables/defaults.
-     */
-    self.setupVariables = function() {
-        //  Set the environment variables we need.
-        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+  /**
+  *  Set up server IP address and port # using env variables/defaults.
+  */
+  self.setupVariables = function() {
+    self.port      = 80;
+  };
 
-        if (typeof self.ipaddress === "undefined") {
-            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
-            //  allows us to run/test the app locally.
-            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-            self.ipaddress = "127.0.0.1";
-        };
+
+  /**
+  *  terminator === the termination handler
+  *  Terminate server on receipt of the specified signal.
+  *  @param {string} sig  Signal to terminate on.
+  */
+  self.terminator = function(sig){
+    if (typeof sig === "string") {
+      console.log('%s: Received %s - terminating app ...',
+        Date(Date.now()), sig);
+        process.exit(1);
+    }
+    console.log('%s: Node server stopped.', Date(Date.now()) );
+  };
+
+
+  /**
+  *  Setup termination handlers (for exit and a list of signals).
+  */
+  self.setupTerminationHandlers = function(){
+    //  Process on exit and signals.
+    process.on('exit', function() { self.terminator(); });
+
+    // Removed 'SIGPIPE' from the list - bugz 852598.
+    ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
+      'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
+      ].forEach(function(element, index, array) {
+        process.on(element, function() { self.terminator(element); });
+      });
+  };
+
+
+  /*  ================================================================  */
+  /*  App server functions (main app logic here).                       */
+  /*  ================================================================  */
+
+  /**
+  *  Create the routing table entries + handlers for the application.
+  */
+  self.createRoutes = function() {
+    self.routes = {};
+
+    self.routes['/'] = function(req, res) {
+      var uuid;
+      uuid = rack();
+      res.writeHead(303, {location: '/openide.html#' + uuid});
+      res.send();
     };
+  };
 
 
-    /**
-     *  Populate the cache.
-     */
-    self.populateCache = function() {
-        if (typeof self.zcache === "undefined") {
-            self.zcache = { 'index.html': '' };
+  /**
+  *  Initialize the server (express) and create the routes and register
+  *  the handlers.
+  */
+  self.initializeServer = function() {
+    self.createRoutes();
+    self.app = express();
+
+    //  Add handlers for the app (from the routes).
+    for (var r in self.routes) {
+      self.app.get(r, self.routes[r]);
+    }
+
+    self.app.use('/queue', kue.app);
+    self.app.use(express.compress());
+
+    self.app.use(express.static(sharejs.scriptsDir));
+    self.app.use(browserChannel({
+      webserver: self.app
+    }, function (client) {
+      var stream;
+      stream = new Duplex({
+        objectMode: true
+      });
+      stream._write = function (chunk, encoding, callback) {
+        if (client.state !== 'closed') {
+          client.send(chunk);
         }
+        return callback();
+      };
+      stream._read = function() {};
+      stream.headers = client.headers;
+      stream.remoteAddress = stream.address;
+      client.on('message', function(data) {
+        return stream.push(data);
+      });
+      stream.on('error', function (msg) {
+        return client.stop();
+      });
+      client.on('close', function (reason) {
+        stream.emit('close');
+        stream.emit('end');
+        return stream.end();
+      });
+      return share.listen(stream);
+    }));
 
-        //  Local cache for static content.
-        self.zcache['index.html'] = fs.readFileSync('./index.html');
-    };
+    self.app.use('/doc', share.rest());
 
+    self.app.use(express.static(__dirname + "/site/"));
 
-    /**
-     *  Retrieve entry (content) from cache.
-     *  @param {string} key  Key identifying content to retrieve from cache.
-     */
-    self.cache_get = function(key) { return self.zcache[key]; };
-
-
-    /**
-     *  terminator === the termination handler
-     *  Terminate server on receipt of the specified signal.
-     *  @param {string} sig  Signal to terminate on.
-     */
-    self.terminator = function(sig){
-        if (typeof sig === "string") {
-           console.log('%s: Received %s - terminating sample app ...',
-                       Date(Date.now()), sig);
-           process.exit(1);
-        }
-        console.log('%s: Node server stopped.', Date(Date.now()) );
-    };
-
-
-    /**
-     *  Setup termination handlers (for exit and a list of signals).
-     */
-    self.setupTerminationHandlers = function(){
-        //  Process on exit and signals.
-        process.on('exit', function() { self.terminator(); });
-
-        // Removed 'SIGPIPE' from the list - bugz 852598.
-        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
-         'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
-        ].forEach(function(element, index, array) {
-            process.on(element, function() { self.terminator(element); });
-        });
-    };
-
-
-    /*  ================================================================  */
-    /*  App server functions (main app logic here).                       */
-    /*  ================================================================  */
-
-    /**
-     *  Create the routing table entries + handlers for the application.
-     */
-    self.createRoutes = function() {
-        self.routes = { };
-
-        self.routes['/'] = function(req, res) {
-            var uuid;
-            uuid = rack();
-            res.writeHead(303, {location: '/openide.html#' + uuid});
-            res.send();
-        };
-    };
-
-
-    /**
-     *  Initialize the server (express) and create the routes and register
-     *  the handlers.
-     */
-    self.initializeServer = function() {
-        self.createRoutes();
-        self.app = express();
-
-        //  Add handlers for the app (from the routes).
-        for (var r in self.routes) {
-            self.app.get(r, self.routes[r]);
-        }
-
-        self.app.use('/queue', kue.app);
-
-        self.app.use(express.static(sharejs.scriptsDir));
-        self.app.use(browserChannel({
-            webserver: self.app
-        }, function (client) {
-            var stream;
-            stream = new Duplex({
-                objectMode: true
-            });
-            stream._write = function (chunk, encoding, callback) {
-                if (client.state !== 'closed') {
-                    client.send(chunk);
-                }
-                return callback();
-            };
-            stream._read = function() {};
-            stream.headers = client.headers;
-            stream.remoteAddress = stream.address;
-            client.on('message', function(data) {
-                return stream.push(data);
-            });
-            stream.on('error', function (msg) {
-                return client.stop();
-            });
-            client.on('close', function (reason) {
-                stream.emit('close');
-                stream.emit('end');
-                return stream.end();
-            });
-            return share.listen(stream);
-        }));
-
-        self.app.use('/doc', share.rest());
-
-        self.app.use(express.static(__dirname + "/site/"));
-
+    mongoclient.connect('mongodb://localhost:27017/outputs', function (err, db) {
+      if (!err) {
         jobs.process('compileAndRun', function (job, done) {
-            var id = job.data.id;
-            var proc;
+          var id = job.data.id;
+          var proc;
+          var name = '/tmp/' + id;
 
-            fs.writeFileSync('/tmp/' + id + '.cpp', job.data.program);
+          fs.writeFileSync('/tmp/' + id + '.cpp', job.data.program);
 
-            proc = spawn('clang++', ['-g', '-O2', '-o', '/tmp/' + id, '/tmp/' + id + '.cpp']);
-            proc.stderr.on('data', function (data) {
-                done(new Error(data));
-            });
-            proc.on('close', function (code) {
-                fs.unlinkSync('/tmp/' + job.data.id + '.cpp');
-            });
-            done();
+          proc = exec('g++ -g -O2 -static -o ' + name + ' ' + name + '.cpp', function (error, stdout, stderr) {
+            if (error) {
+              done(new Error(stderr));
+            } else {
+              var run_proc;
+              run_proc = spawn('./run', [name]);
+              // Write the user's input to stdin
+              run_proc.stdin.write(job.data.input);
+              // Flush the stream by EOF
+              run_proc.stdin.end();
+              // The output comes in chunks, so we concatenate them
+              run_proc.stdout.on('data', function (data) {
+                db.collection('outputs', function (err, collection) {
+                  var doc = {
+                    id: job.id,
+                    output: data.toString()
+                  };
+                  collection.insert(doc, {w:1}, function (err, result) {
+                    if (err) {
+                      console.log(err, result);
+                    }
+                  });
+                });
+              });
+              run_proc.on('close', function () {
+                done();
+              });
+            }
+          });
         });
-    };
-
-
-    /**
-     *  Initializes the sample application.
-     */
-    self.initialize = function() {
-        self.setupVariables();
-        self.populateCache();
-        self.setupTerminationHandlers();
-
-        // Create the express server and routes.
-        self.initializeServer();
-    };
-
-
-    /**
-     *  Start the server (starts up the sample application).
-     */
-    self.start = function() {
-        //  Start the app on the specific interface (and port).
-        self.app.listen(self.port, self.ipaddress, function() {
-            console.log('%s: Node server started on %s:%d ...',
-                        Date(Date.now() ), self.ipaddress, self.port);
+        self.app.get('/check', function(req, res) {
+          db.collection('outputs', function (err, collection) {
+            collection.find({
+              id: req.query.id
+            }).toArray(function(err, docs) {
+              var output = "", i;
+              for (i = 0; i < docs.length; i++) {
+                output += docs[i].output;
+              }
+              res.send(output);
+              collection.remove({
+                id: req.query.id
+              }, function (err, num) {
+                if (err) throw new Error(err);
+              });
+            });
+          });
         });
-    };
+      }
+    });
+  };
 
-};   /*  Sample Application.  */
+
+  /**
+  *  Initializes the application.
+  */
+  self.initialize = function() {
+    self.setupVariables();
+    self.setupTerminationHandlers();
+
+    // Create the express server and routes.
+    self.initializeServer();
+  };
+
+
+  /**
+  *  Start the server.
+  */
+  self.start = function() {
+    //  Start the app on the specific interface (and port).
+    self.app.listen(self.port);
+  };
+
+};
 
 
 
 /**
- *  main():  Main code.
- */
-var zapp = new SampleApp();
+*  main():  Main code.
+*/
+var zapp = new App();
 zapp.initialize();
 zapp.start();
 
--- site/openide.js
+++ site/openide.js
@@ -1,108 +1,121 @@
 $(function () {
-    "use strict";
-    var editor = ace.edit("editor"), textarea = $("#program"), form = $("#form"), editor_el = $("#editor"), submit_button = $("#submit"), output = $("#output"), timemem = $("#time_mem"), share = $("#share"), file = $("#file"), file_tip = $("#file_tip"), id, address = $("#url"), cover = $("#cover"), dialog = $("#dialog"),s = new BCSocket(null, {reconnect: true}), conn = new sharejs.Connection(s), doc, jobid, gotten = false;
-    editor.getSession().setMode("ace/mode/c_cpp");
-    submit_button.click(function () {
-        submit_button.prop('disabled', true).addClass("disabled").removeClass("enabled");
-        $.ajax({
-            url: "/queue/job",
-            method: "POST",
-            data: {
-                "type": "compileAndRun",
-                "data": {
-                    "id": id,
-                    "program": editor.getSession().getValue(),
-                    "input": $("#input").val()
-                }
-            },
-            success: function (data) {
-                if (data.message === "job created") {
-                    if (data.id) {
-                        jobid = data.id;
-                        setTimeout(check, 1000);
-                    }
-                }
-            }
-        });
+  "use strict";
+  var editor = ace.edit("editor"), textarea = $("#program"), form = $("#form"), editor_el = $("#editor"), submit_button = $("#submit"), output = $("#output"), timemem = $("#time_mem"), share = $("#share"), file = $("#file"), file_tip = $("#file_tip"), id, address = $("#url"), cover = $("#cover"), dialog = $("#dialog"),s = new BCSocket(null, {reconnect: true}), conn = new sharejs.Connection(s), doc, jobid, gotten = false, body = $("body");
+  editor.getSession().setMode("ace/mode/c_cpp");
+  id = location.hash.slice(1);
+  if (!id) {
+    location.href = '/';
+  }
+  editor.focus();
+  doc = conn.get('programs', id);
+  doc.subscribe();
+  doc.whenReady(function () {
+    body.removeClass("wait");
+    if (!doc.type) {
+      doc.create('text');
+    }
+    if (doc.type && doc.type.name === 'text') {
+      doc.attach_ace(editor);
+      submit_button.prop('disabled', false).addClass("enabled").removeClass("disabled");
+      if (doc.snapshot.length === 0) {
+        editor.getSession().setValue("#include <iostream>\n#include <algorithm>\n\nusing namespace std;\n\nint main() {\n    cout << \"Hello world!\" << endl;\n    return 0;\n}");
+      }
+    }
+  });
+  submit_button.click(function () {
+    body.addClass("wait");
+    submit_button.prop('disabled', true).addClass("disabled").removeClass("enabled");
+    $.ajax({
+      url: "/queue/job",
+      method: "POST",
+      data: {
+        "type": "compileAndRun",
+        "data": {
+          "id": id,
+          "program": editor.getSession().getValue(),
+          "input": $("#input").val()
+        }
+      },
+      success: function (data) {
+        if (data.message === "job created") {
+          if (data.id) {
+            jobid = data.id;
+            setTimeout(check, 1000);
+          }
+        }
+      }
     });
-    function check() {
-        $.ajax({
-            url: "/queue/job/" + jobid,
-            method: "GET",
-            success: function (data) {
-                if (data.state !== 'active') {
-                    $.ajax({
-                        url: "/queue/job/" + jobid + "/log",
-                        method: "GET",
-                        success: function (data) {
-                            output.text(data[0]);
-                        }
-                    });
+  });
+  function check() {
+    $.ajax({
+      url: "/queue/job/" + jobid,
+      method: "GET",
+      success: function (data) {
+        if (data.state !== 'active' || !data) {
+          if (data.state === 'failed') {
+            timemem.empty();
+            output.text(data.error.substr(0, data.error.indexOf("at /home")));
+            submit_button.prop('disabled', false).addClass("enabled").removeClass("disabled");
+            body.removeClass("wait");
+          } else {
+            $.ajax({
+              url: "/check",
+              method: "GET",
+              data: {
+                "id": jobid
+              },
+              success: function (data) {
+                var lines = data.split("\n"), tmidx = data.indexOf("Time used");
+                submit_button.prop('disabled', false).addClass("enabled").removeClass("disabled");
+                body.removeClass("wait");
+                if (tmidx === -1) {
+                  output.text(data);
+                } else if (lines[0].indexOf("Error") !== -1) {
+                  output.empty();
+                  output.append($("<b>").addClass("red").text(lines[0]));
+                  timemem.html(lines[2] + "<br>" + lines[3]);
+                } else if (lines[0].length === 0) {
+                  output.text("Your program outputted nothing.");
+                  timemem.html(lines[1] + "<br>" + lines[2]);
                 } else {
-                    setTimeout(check, 1000);
+                  output.text(data.substr(0, tmidx));
+                  timemem.html(data.substr(tmidx).replace(/\n/g, "<br>"));
                 }
-            }
-        });
-        /*
-        var lines = data.split("\n"), tmidx = data.indexOf("Time used");
-        submit_button.prop('disabled', false).addClass("enabled").removeClass("disabled");
-        if (tmidx === -1) {
-            output.text(data);
-        } else if (lines[0].indexOf("Error") !== -1) {
-            output.empty();
-            output.append($("<b>").addClass("red").text(lines[0]));
-            timemem.html(lines[2] + "<br>" + lines[3]);
-        } else if (lines[0].length === 0) {
-            output.empty();
-            output.text("Your program outputted nothing.");
-            timemem.html(lines[1] + "<br>" + lines[2]);
+              }
+            });
+          }
         } else {
-            output.text(data.substr(0, tmidx));
-            timemem.html(data.substr(tmidx).replace(/\n/g, "<br>"));
+          setTimeout(check, 1000);
         }
-        */
-    }
+      }
+    });
+  }
 
-    editor.focus();
-    id = location.hash.slice(1);
-    doc = conn.get('programs', id);
-    doc.subscribe();
-    doc.whenReady(function () {
-        if (!doc.type) {
-            doc.create('text');
-        }
-        if (doc.type && doc.type.name === 'text') {
-            doc.attach_ace(editor);
-            if (doc.snapshot.length === 0) {
-                editor.getSession().setValue("#include <iostream>\n\nusing namespace std;\n\nint main() {\n    cout << \"Hello world!\" << endl;\n    return 0;\n}");
-            }
-        }
-    });
-    if (window.FileReader) {
-        file.on("change", function (event) {
-            var f = event.target.files[0]; 
+  if (window.FileReader) {
+    file.on("change", function (event) {
+      var f = event.target.files[0]; 
 
-            if (!f) {
-                file_tip.text("Failed to load file.");
-            } else {
-                var r = new FileReader();
-                r.onload = function (event) {
-                    var contents = event.target.result;
-                    editor.getSession().setValue(contents);
-                };
-                r.readAsText(f);
-            }
-        });
-    } else {
-        $("#file_wrapper").hide();
-    }
-    share.click(function () {
-        cover.show();
-        dialog.show();
-        address.val(location.href);
+      if (!f) {
+        file_tip.text("Failed to load file.");
+      } else {
+        var r = new FileReader();
+        r.onload = function (event) {
+          var contents = event.target.result;
+          editor.getSession().setValue(contents);
+        };
+        r.readAsText(f);
+      }
     });
-    $("#done").click(function () {
-        cover.hide();
-        dialog.hide();
-    });
+  } else {
+    $("#file_wrapper").hide();
+  }
+  share.click(function () {
+    cover.show();
+    dialog.show();
+    address.val(location.href);
+  });
+  $("#done").click(function () {
+    cover.hide();
+    dialog.hide();
+  });
 });
